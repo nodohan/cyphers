c5a2034372d29db7c8fba78b707285aae1234225
nodohan <dododosa@naver.com>
2021-10-11 22:10:21 +0900

    랭킹 수집 크롤링 기능 개발 완료 + 스케쥴링 적용

--- server.js
+++ server.js
@@ -298,6 +298,13 @@ app.get('/insertMatches', function(req, res) {
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+var rankSchedulerTime = "00 13 * * *";
+scheduler.scheduleJob(rankSchedulerTime, function() {
+    logger.info("call rank scheduler");
+    getRanks();
+    logger.info("end rank scheduler");
+});
+
 app.get('/getHtml', function(req, res) {
     let allowIps = ["localhost", "127.0.0.1", "221.143.115.91", ":114.207.113.136", "::1", "::ffff:127.0.0.1", "34.64.4.116"];
     const ip = req.headers['x-forwarded-for'] || req.ip;
@@ -314,39 +321,87 @@ app.get('/getHtml', function(req, res) {
             .end();
     }
 
-    return getHtml()
-        .then(html => {
-            let rankList = [];
-            const $ = cheerio.load(html.data);
-            const $bodyList = $(".total_rank tbody tr");
+    getRanks();
 
-            $bodyList.each(function(i, row) {
-
-                let rank = $($(row).find("td").get(0)).clone();
-                rank.find(".chg_num").remove();
-
-                rankList[i] = {
-                    rank: rank.text().replace(/\t/gi, "").replace("\n", ""),
-                    name: $($(row).find("td").get(1)).find("a").text(),
-                    rp: $($(row).find("td").get(3)).text()
-                };
-            });
-
-            return rankList;
-        })
-        .then(res => insertRank(res));;
+    return res
+        .status(200)
+        .send(true)
+        .end();
 });
 
-const getHtml = async() => {
+async function getRanks() {
+    let run = true;
+    var todayYYYYMMDD = getYYYYMMDD();
+
+    logger.debug("today string %s", todayYYYYMMDD);
+
+    for (let i = 1; i <= 130; i++) {
+        if (run) {
+            //logger.debug("crawling page number %d", i);
+            await getHtml(i)
+                .then(html => {
+                    let rankList = [];
+                    const $ = cheerio.load(html.data);
+                    const $bodyList = $(".total_rank tbody tr");
+
+                    if ($bodyList.length != 50) {
+                        logger.debug("랭킹 크롤링 끝")
+                        run = false;
+                    }
+
+                    $bodyList.each(function(i, row) {
+
+                        let rank = $($(row).find("td").get(0)).clone();
+                        rank.find(".chg_num").remove();
+
+                        rankList[i] = {
+                            today: todayYYYYMMDD,
+                            rank: rank.text().replace(/\t/gi, "").replace("\n", ""),
+                            name: $($(row).find("td").get(1)).find("a").text(),
+                            rp: $($(row).find("td").get(3)).text()
+                        };
+                    });
+
+                    return rankList;
+                })
+                .then(jsonList => insertRank(jsonList));;
+        }
+    }
+}
+
+const getHtml = async(page) => {
     try {
-        return await axios.get("http://cyphers.nexon.com/cyphers/article/ranking/total/19/1");
+        return await axios.get("http://cyphers.nexon.com/cyphers/article/ranking/total/19/" + page);
     } catch (error) {
         logger.error(error);
     }
 };
 
-function insertRank(resultJson) {
-    logger.debug(JSON.stringify(resultJson));
+async function insertRank(jsonList) {
+    //logger.debug(JSNO.stringify(jsonList));
+    let result = 0;
+    pool = pool || (await createPoolAndEnsureSchema());
+
+    jsonList.forEach(async function(json) {
+        try {
+            if (json.rp == '') {
+                return;
+            }
+
+            let query = "INSERT INTO rank (rankDate, rankNumber, playerId, nickname, season) "
+            query += " SELECT '" + json.today + "', '" + json.rank + "', nick.playerId, '" + json.name + "' , '2021U' "
+            query += " FROM nickNames nick"
+            query += " WHERE nick.nickname = '" + json.name + "' ";
+
+            //logger.debug(query);
+
+            result += await pool.query(query);
+        } catch (err) {
+            logger.error(err);
+        }
+    })
+
+    return result == jsonList.length;
 }
 
 async function insertMatches(res) {
@@ -514,4 +569,9 @@ function addDays(date, days) {
 
 function getMinDay(date1, date2) {
     return date1.getTime() < date2.getTime() ? date1 : date2;
+}
+
+function getYYYYMMDD() {
+    var rightNow = new Date();
+    return rightNow.toISOString().slice(0, 10).replace(/-/g, "");
 }
