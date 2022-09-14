e133f2c69580ad7930c1240c7343577e4d609567
nodohan <dododosa@naver.com>
2022-09-15 00:37:07 +0900

    2022H 시즌 결산 메뉴 개발 진행 중

--- server/controller/statsController.js
+++ server/controller/statsController.js
@@ -37,16 +37,46 @@ module.exports = (scheduler, maria, acclogger) => {
     });
 
     app.get('/statsList', async function(req, res) {
-        let today = new Date();
-        if (today.getHours() <= 2) {
-            today = commonUtil.addDays(today, -1);
+        let todayStr = '';
+
+        if (req.query.season != null) {
+            todayStr = req.query.season;
+        } else {
+            let today = new Date();
+            if (today.getHours() <= 2) {
+                today = commonUtil.addDays(today, -1);
+            }
+            todayStr = commonUtil.getYYYYMMDD(today, false);
         }
 
-        let todayStr = commonUtil.getYYYYMMDD(today, false);
         pool = await maria.getPool();
 
         try {
             let query = " SELECT * FROM match_stats WHERE statsDate = '" + todayStr + "' ";
+            logger.debug(query);
+
+            let row = await pool.query(query);
+            res.send({ 'row': row });
+        } catch (err) {
+            logger.error(err);
+            return res
+                .status(500)
+                .send('오류 발생')
+                .end();
+        }
+    });
+
+    app.get('/stats2022h', function(req, res) {
+        res.render('./pc/stats2022h');
+    });
+
+    app.get('/statsSeasonList', async function(req, res) {
+        let season = req.query.season
+
+        pool = await maria.getPool();
+
+        try {
+            let query = ` SELECT * FROM match_stats WHERE statsDate = '${season}' `;
             logger.debug(query);
 
             let row = await pool.query(query);
