b734669e5623eafc1e3ba54171b6640b4f91bfb9
nodohan <dododosa@naver.com>
2022-09-30 01:04:21 +0900

    포지션 통계: 파일명에 js 누락분 처리 밑 쿼리 수정 -> 캐릭터명을 파라미터로 받도록

--- server/controller/positionController.js
+++ server/controller/positionController.js
@@ -16,11 +16,16 @@ module.exports = (scheduler, maria, acclogger) => {
 
     app.get('/positionAttrList', async function(req, res) {
         //let todayStr = commonUtil.getYYYYMMDD(new Date(), false);
-        let todayStr = '2022-09-27';
+        let charName = req.query.charName;
+
+        let todayStr = '2022-09-29';
         pool = await maria.getPool();
 
         try {
-            let query = `select * from position_attr_stats where checkDate = '${todayStr}' `;
+            let query = `select * 
+                        from position_attr_stats
+                        where checkDate = '${todayStr}'
+                        and charName = '${charName == null ? 'all' : charName}' `;
             logger.debug(query);
 
             let row = await pool.query(query);
