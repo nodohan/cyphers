6834540cf2a32a005b6918a911c8381bedd857ed
nodohan <dododosa@naver.com>
2022-12-25 21:25:16 +0900

    랭킹 수집 후 처리 방식 변경 - 기존: 수집된 랭킹 사용자의 nickname을 nickName 테이블에서 각각 조회하여 insert - 변경: rank_sync 테이블을 도입하여 테이블에 bulk insert 로 변경 후  beforeRank 를 이용하여 1차 처리 후  before가 없는 사용자는 nickName테이블에서 조회하여 처리
    * 이렇게 처리하여도 신규 사용자 또는 휴면된 사용자가 재 랭킹진입 시 처리가 안되는 문제가 있는데 이건 많은 수는 아니여서 일딴 추후 처리
    - 네오플api에서 playerId를 닉네임으로 검색해서 가져오면 되긴 하는데 로직을 더 많이 도입해야 하다보니 고민됨.

--- server/scheduler/rankCrawlingScheduler.js
+++ server/scheduler/rankCrawlingScheduler.js
@@ -1,10 +1,13 @@
+const logger = require('../../config/winston.js');
+const commonUtil = require('../util/commonUtil');
+//const rankSampleJson = require('../../sample/rankSample.js');
+
 module.exports = (scheduler, maria) => {
     const app = require('express').Router();
 
     const axios = require("axios");
-    const cheerio = require("cheerio");
+    //const cheerio = require("cheerio");
     const myConfig = require('../../config/config.js');
-
 
     var rankSchedulerTime = "00 13 * * *";
     //var rankSchedulerTime = "08 20 * * *";
@@ -41,53 +44,127 @@ module.exports = (scheduler, maria) => {
             .end();
     });
 
+    // 더미데이터로 테스트
+    app.get('/getHtmlByJson', function(req, res) {
+        let allowIps = ["localhost", "127.0.0.1", "221.143.115.91", ":114.207.113.136", "::1", "::ffff:127.0.0.1", "34.64.4.116"];
+        const ip = req.headers['x-forwarded-for'] || req.ip;
+
+        if (ip.indexOf(",") > 0) {
+            ip = ip.toString().split(",")[1].trim();
+        }
+
+        logger.debug("call getHtml ip", ip);
+        if (!allowIps.includes(ip)) {
+            return res
+                .status(403)
+                .send('Not allow IP :' + ip + ' \n')
+                .end();
+        }
+
+        var todayYYYYMMDD = commonUtil.getYYYYMMDD();
+        //insertRankSync(rankSampleJson.map(row => [todayYYYYMMDD, row.ranking, row.beforeRank, row.mNickname, row.ratingPoint]));
+
+        return res
+            .status(200)
+            .send(true)
+            .end();
+    });
+
     async function getRanks() {
-        let run = true;
-        var todayYYYYMMDD = getYYYYMMDD();
+        /***
+         * 1. cyphers 홈페이지에서 rank 받아옴
+         * 2. rank_sync 테이블에 insert 
+         * 3. rank_sync에서 userRank테이블로 복사
+         *   조건1) rank 테이블에 insert select 하는데 beforerank가 0이 아닌 항목만 join해서 playerId를 받아와서처리 
+         *   조건2) beforerank가 0인경우 nickname 테이블에서 가져와서 처리
+         */
 
-        logger.debug("today string %s", todayYYYYMMDD);
+        var todayStr = commonUtil.getYYYYMMDD(new Date(), false);
+        var yesterdayStr = commonUtil.getYYYYMMDD(commonUtil.addDays(new Date(), -1), false);
 
-        for (let i = 1; i <= 140; i++) {
-            //for (let i = 1; i <= 1; i++) {
-            if (run) {
-                let html = await getHtml(i);
+        logger.debug("today string %s", todayStr);
 
-                /*{
-                    curDate: '2022-12-16',
-                    ranking: 5306,
-                    beforeRank: 5256,
-                    ratingPoint: 1262,
-                    mGrade: 92,
-                    mNickname: '가나다라마바사',
-                    clanName: '어쩌고저쩌고'
-                  } */
-                //console.log(html);
-                let rankList = [];
-                const $bodyList = html.data.rankingTopResponses;
-
-                //console.log($bodyList);
-                if ($bodyList.length < 50) {
-                    logger.debug("랭킹 크롤링 끝");
-                    run = false;
-                }
-
-                $bodyList.forEach(function(row, i) {
-                    //console.log(row);
-
-                    rankList[i] = {
-                        today: todayYYYYMMDD,
-                        rank: row.ranking,
-                        name: row.mNickname,
-                        //name: $($(row).find("td").get(1)).text(),
-                        rp: row.ratingPoint
-                    };
-                });
-                //logger.debug("랭킹 페이지 %s! insert 시작 ", i);
-                await insertRank(rankList);
-            }
+        try {
+            let rankList = getRankList(); // 1.
+            await insertRankSync(rankList); // 2. 
+            await insertRankBySync(todayStr, yesterdayStr); // 3.
+        } catch (err) {
+            logger.error("insert rank fail");
+            logger.error(err.message);
         }
     }
 
+    async function insertRankBySync(today, yesterday) {
+        let hasBefore = `
+            INSERT INTO userRank
+            SELECT 
+                '${today}', sy.rankNumber, ur.playerId, sy.nickname, '2022H', sy.rp  
+            FROM rank_sync sy
+            INNER JOIN (
+                SELECT playerId, rankNumber, nickname 
+                FROM userRank WHERE rankDate = '${yesterday}'
+            ) ur ON ur.rankNumber = sy.ranknumberbefore AND ur.nickname = sy.nickname 
+            WHERE sy.ranknumberbefore != 0`;
+        logger.debug(hasBefore);
+
+        let emtpyBefore = `
+            INSERT INTO userRank
+            SELECT                 
+               STRAIGHT_JOIN '${today}', sy.rankNumber, ln.playerId, sy.nickname, '2022H', sy.rp 
+            FROM (
+               SELECT nm.playerId, nm.nickname FROM nickNames nm
+               INNER JOIN ( 
+                   SELECT playerId, MAX(checkingDate) checkingDate FROM nickNames GROUP BY playerId  
+               ) lastNickname ON lastNickname.playerId = nm.playerId AND lastNickname.checkingDate = nm.checkingDate
+            ) ln
+            INNER JOIN ( SELECT * FROM rank_sync WHERE ranknumberbefore = 0 ) sy  ON sy.nickname = ln.nickname`
+        logger.debug(emtpyBefore);
+
+        let pool = await maria.getPool();
+
+        //어제 랭킹이 존재하는사람
+        await pool.query(hasBefore);
+
+        //어제 랭킹이 존재하지 않는사람 ( ranknumberbefore = 0)
+        await pool.query(emtpyBefore);
+
+        // 이렇게 해도 닉변후에 랭킹이 사라진 후 다시 랭킹에 들어오게 되면 누락될 수 있음.
+        // 이런 건은 차후 해결 하기로 함. 
+
+        logger.debug("end insertRankBySync");
+    }
+
+    async function getRankList() {
+        let run = true;
+        let rankList = [];
+        for (let i = 1; i <= 120; i++) {
+            //for (let i = 1; i <= 2; i++) {
+            if (run) {
+                let html = await getHtml(i);
+                html.data.rankingTopResponses.forEach(row => rankList.push([todayYYYYMMDD, row.ranking, row.beforeRank, row.mNickname, row.ratingPoint]));
+                if (rankList.length < 50) {
+                    logger.debug("랭킹 크롤링 끝");
+                    run = false;
+                }
+            }
+        }
+
+        console.log(rankList);
+        return rankList;
+    }
+
+    /***
+     * 
+        [{
+            curDate: '2022-12-16',
+            ranking: 5306,
+            beforeRank: 5256,
+            ratingPoint: 1262,
+            mGrade: 92,
+            mNickname: '가나다라마바사',
+            clanName: '어쩌고저쩌고'
+           }] 
+    */
     const getHtml = async(page) => {
         try {
             return await axios.get("http://cyphers.nexon.com/ranking/total/21?page=" + page);
@@ -96,32 +173,24 @@ module.exports = (scheduler, maria) => {
         }
     };
 
-    async function insertRank(jsonList) {
-        var pool = await maria.getPool();
-        let result = 0;
+    async function insertRankSync(arrList) {
+        logger.debug("insert ranking length = " + arrList.length);
 
-        jsonList.forEach(async function(json) {
-            let query = ` INSERT INTO userRank (rankDate, rankNumber, playerId, nickname, season, rp) 
-                 SELECT '${json.today}', '${json.rank}', nick.playerId, '${json.name}' , '2022U', ${json.rp} 
-                 FROM nickNames nick 
-                 WHERE nick.nickname = '${json.name}' ORDER BY checkingDate DESC LIMIT 1 `;
+        let pool = await maria.getPool();
+        pool.query("DELETE FROM rank_sync ");
 
-            logger.debug(query);
-            try {
-                result += await pool.query(query);
-            } catch (err) {
-                //logger.error(err);
-                console.log(err);
-            }
+        let query = ` INSERT INTO rank_sync 
+                          (rankDate, rankNumber, rankNumberBefore, nickname, rp) 
+                      VALUES
+                          ( ?, ?, ?, ?, ? ) `;
+
+        pool.batch(query, arrList, function(err) {
+            console.log(err);
+            logger.error(err.message);
         });
 
-        pool.end();
-        return result == jsonList.length;
-    }
+        logger.debug("insert ranking end");
 
-    function getYYYYMMDD() {
-        var rightNow = new Date();
-        return rightNow.toISOString().slice(0, 10).replace(/-/g, "");
     }
 
     return app;
