ac0cff188843b00a0e87f942a36a37d6cbb97361
nodohan <dododosa@naver.com>
2023-03-01 23:35:34 +0900

    사용자 검색 > "전시즌" 랭킹 표기 기능 2022U 포함 하도록 추가

--- server/controller/userSearchController.js
+++ server/controller/userSearchController.js
@@ -110,7 +110,7 @@ module.exports = (scheduler, maria, acclogger) => {
 				, IF(pl.privateYn = 'Y', '몰?루?', ur.rankNumber) rankNumber 
 			FROM userRank ur 
 			LEFT JOIN player pl ON pl.playerId = ur.playerId 
-			WHERE rankDate in ( '2022-02-17', '2022-08-18' ) AND pl.playerId = '${playerId}' `;
+			WHERE rankDate in ( '2022-02-17', '2022-08-18', '2023-02-23' ) AND pl.playerId = '${playerId}' `;
 
         pool = await maria.getPool();
         try {
