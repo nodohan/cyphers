de3f718288dba59c77b36bb6293e6a885ab32b3f
nodohan <dododosa@naver.com>
2023-07-02 11:44:39 +0900

    공통 ajax 로직에서 로딩바 종료 로직 추가

--- js/common.js
+++ js/common.js
@@ -5,6 +5,12 @@ function ajaxCall(url, data, callback) {
         url: url,
         data: data,
         success: function(data) {
+            try {
+                callLoadingBar(false);
+            } catch (e) {
+
+            }
+
             if (typeof callback == 'function') {
                 if (data.resultCode == 200) {
                     callback(data);
@@ -14,10 +20,11 @@ function ajaxCall(url, data, callback) {
             }
         },
         error: function(data) {
+            callLoadingBar(false);
             return;
         }
     }).done(function() {
-
+        callLoadingBar(false);
     });
     return result;
 }
