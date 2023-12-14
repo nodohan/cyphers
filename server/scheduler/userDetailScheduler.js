d0dab8a8b7144d6c466e2313060b714c5e922200
DHROH-NB1\DHROH <dhroh@nc-and.com>
2023-12-14 13:43:19 +0900

    [modify] 기존 사용자 비교 로직 삭제  같은팀일때 적팀일때 비교하는 방식으로 수정개발

--- server/scheduler/userDetailScheduler.js
+++ server/scheduler/userDetailScheduler.js
@@ -10,7 +10,7 @@ module.exports = (scheduler, maria, acclogger) => {
 
   app.use(acclogger());
   
-  // URL: /userDetail/insertUserDetail
+  // URL: http://localhost:8080/userDetail/insertUserDetail
   app.get('/insertUserDetail', function(req, res) {
     doDayWork('2023-12-11');
   });
@@ -23,44 +23,22 @@ module.exports = (scheduler, maria, acclogger) => {
     // const userList = await matchDetailCharRepository.selectMatchDetailByMatchDate(yyyymmdd); 
 
     // 날짜 | 내 아이디 | 상대방 아이디 | 같은팀으로 | 같은팀으로 패 | 적팀으로 내가이김 | 적팀으로 내가 짐
-    mergeMatchUser2(arr, '1826e7c7f0becbc1e65ee644c28f0072');
+    //mergeMatchUser(arr, '1826e7c7f0becbc1e65ee644c28f0072');
+    mergeMatchUser(arr, '18d5b7064acd4d0a667cac264026cfa3');
 
 
   }
   
-  const mergeMatchUser = (jsonArr, userId) => {
-    const playerCount = new Map();
-
-    jsonArr.forEach(row => {
-      const isWin = row.teams[0].players.includes(userId);
-      const winningTeam = isWin ? row.teams[0].players : row.teams[1].players;
-      const losingTeam = isWin ? row.teams[1].players : row.teams[0].players;
-
-      winningTeam.forEach(player => {
-        playerCount.set(player, {
-          win: (playerCount.get(player)?.win || 0) + 1,
-          lose: playerCount.get(player)?.lose || 0,
-        });
-      });
-
-      losingTeam.forEach(player => {
-        playerCount.set(player, {
-          win: playerCount.get(player)?.win || 0,
-          lose: (playerCount.get(player)?.lose || 0) + 1,
-        });
-      });
-    });
-
-    console.log(Object.fromEntries(playerCount));
-    return Object.fromEntries(playerCount);
-  };
-
-  const mergeMatchUser2 = (jsonArr, userId) => {
+  const mergeMatchUser = (jsonArr, playerId) => {
     const myTeam = new Map();
     const enemyTeam = new Map();
 
     jsonArr.forEach(row => {
-        const isWin = row.teams[0].players.includes(userId);
+        if(!(row.teams[0].players.includes(playerId) || row.teams[1].players.includes(playerId))) {
+          return ;
+        }
+
+        const isWin = row.teams[0].players.includes(playerId);
         const winningTeam = isWin ? row.teams[0].players : row.teams[1].players;        
         
         row.players.forEach(player => {
