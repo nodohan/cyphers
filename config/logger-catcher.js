f753a8da1155e09161b7a251d93ebd91608fdc70
nodohan <dhroh@nc-and.com>
2025-02-07 16:41:32 +0900

    [modify] 로컬 개발환경에서는 로그를 쌓지 않도록 수정 + xss 방지 로직 추가

--- config/logger-catcher.js
+++ config/logger-catcher.js
@@ -7,6 +7,9 @@ module.exports = () => {
     const historyRepository = new repository(mariadb);
     const insertHistory =  (req) => {
         try {
+            if(process.env.MOD == "LOCAL") {
+                return;
+            }
             const url = req.url;
             if (url != "/" 
                 && !url.startsWith('/view') 
@@ -15,13 +18,22 @@ module.exports = () => {
                 && !url.startsWith('/bootstrap') ) {
                 logger.info('%s %s', req.method, req.url);
                 const ip = commonUtil.getIp(req);
-                historyRepository.insertAccessLog(ip,req.url);
+                const url = sanitizeURL(decodeURI(req.url));                
+                historyRepository.insertAccessLog(ip,url);
             }
         } catch(err) {
             console.log(err);
         }
     }
 
+    const sanitizeURL = (url) => {
+        return url.replace(/</g, "&lt;")
+                  .replace(/>/g, "&gt;")
+                  .replace(/"/g, "&quot;")
+                  .replace(/'/g, "&#x27;")
+                  .replace(/&/g, "&amp;");
+    }
+    
     return (req, rep, next) => {        
         insertHistory(req);
         // 미들웨어나 router 콜백에서 next 또는 response에 대한 응답을 생략하면 클라이언트가 계속 대기하게 되므로 꼭 잊지 말 것.
