6dfd4cad8478e659d98d130da92d045dff0554c8
nodohan <dhroh@nc-and.com>
2025-04-10 12:53:07 +0900

    [modify] 2025H 시즌 오픈
    (cherry picked from commit 3a47c13fdec583a48a54c60a479b59395bae9e72)

--- server/controller/userSearchController.js
+++ server/controller/userSearchController.js
@@ -149,7 +149,7 @@ module.exports = (scheduler, maria, acclogger) => {
 				, IF(pl.privateYn = 'Y', '몰?루?', ur.rankNumber) rankNumber 
 			FROM userRank ur 
 			LEFT JOIN player pl ON pl.playerId = ur.playerId 
-			WHERE rankDate in ( '2022-02-17', '2022-08-18', '2023-02-23', '2023-09-14', '2024-03-21', '2024-09-26' ) AND pl.playerId = '${playerId}' `;
+			WHERE rankDate in ( '2022-02-17', '2022-08-18', '2023-02-23', '2023-09-14', '2024-03-21', '2024-09-26', '2025-04-10' ) AND pl.playerId = '${playerId}' `;
 
         pool = await maria.getPool();
         try {
