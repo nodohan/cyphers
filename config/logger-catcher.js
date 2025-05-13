e86efc2c904ff11e0f922d0b1cadc13b7a232b19
nodohan <dhroh@nc-and.com>
2025-05-13 15:21:59 +0900

    [feat] SQL Injection 방어코드 추가

--- config/logger-catcher.js
+++ config/logger-catcher.js
@@ -4,28 +4,60 @@ const commonUtil = require('../server/util/commonUtil');
 
 module.exports = () => {
 
+    const suspiciousPatterns = [
+        /(\%27)|(\')|(\-\-)|(\%23)|(#)/i,                        // ', --, # 주석
+        /(\%22)|(\")/i,                                          // "
+        /\b(OR|AND)\b.+?=/i,                                     // OR a=1, AND a=1
+        /\bUNION\b/i,
+        /\bSELECT\b/i,
+        /\bINSERT\b/i,
+        /\bUPDATE\b/i,
+        /\bDELETE\b/i,
+        /\bDROP\b/i,
+        /\bSLEEP\s*\(/i,
+        /\bBENCHMARK\s*\(/i,
+        /;.*$/i,                                                 // 끝에 ; 넣는 시도
+      ];
     const historyRepository = new repository(mariadb);
-    const insertHistory =  (req) => {
-        try {
-            if(process.env.MOD != "REAL") {
-                return;
-            }
-            const url = req.url;
-            if (url != "/" 
-                && !url.startsWith('/view') 
-                && !url.startsWith('/css') 
-                && !url.startsWith('/js') 
-                && !url.startsWith('/bootstrap') ) {
-                logger.info('%s %s', req.method, req.url);
-                const ip = commonUtil.getIp(req);
-                const url = sanitizeURL(decodeURI(req.url));                
-                historyRepository.insertAccessLog(ip,url);
-            }
-        } catch(err) {
-            console.log(err);
-        }
-    }
 
+    const isSuspicious = (url) => {
+        const decoded = decodeURIComponent(url);
+        return suspiciousPatterns.some((pattern) => pattern.test(decoded));
+    };
+    
+    const insertHistory = (req, res) => {
+        try {
+          if (process.env.MOD !== 'REAL') return false;
+      
+          const url = decodeURIComponent(req.url);
+      
+          if (isSuspicious(url)) {
+            const ip = commonUtil.getIp(req);
+            logger.warn(`[SQL INJECTION] IP: ${ip}, URL: ${url}`);
+            res.status(403).send('Forbidden');  // ← 여기를 위해 res 필요함
+            return true;  // 차단됨
+          }
+      
+          // 일반 로깅
+          if (url !== '/' &&
+              !url.startsWith('/view') &&
+              !url.startsWith('/css') &&
+              !url.startsWith('/js') &&
+              !url.startsWith('/bootstrap')) {
+            logger.info('%s %s', req.method, req.url);
+            const ip = commonUtil.getIp(req);
+            const safeUrl = sanitizeURL(url);
+            historyRepository.insertAccessLog(ip, safeUrl);
+          }
+      
+          return false; // 정상 흐름
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+    };
+      
+      
     const sanitizeURL = (url) => {
         return url.replace(/</g, "&lt;")
                   .replace(/>/g, "&gt;")
@@ -34,11 +66,12 @@ module.exports = () => {
                   .replace(/&/g, "&amp;");
     }
     
-    return (req, rep, next) => {        
-        insertHistory(req);
-        // 미들웨어나 router 콜백에서 next 또는 response에 대한 응답을 생략하면 클라이언트가 계속 대기하게 되므로 꼭 잊지 말 것.
-        next();
-    }
+    return (req, res, next) => {
+        const blocked = insertHistory(req, res);
+        if (!blocked){
+            next();  // 공격 감지된 경우 next() 호출하지 않음
+        }
+    };
 
 };
 
