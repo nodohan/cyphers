2ffe419afbb866f82545a97fd535d837208401dc
nodohan <dododosa@naver.com>
2026-03-22 21:29:06 +0900

    [feat] 메인페이지 내 통계 변경 - 기존 : 조합통계 -탱커/딜러 - 변경: 600위내 캐릭별 전적 추출 및 캐릭별 top5 유저

--- server/controller/statsController.js
+++ server/controller/statsController.js
@@ -1,18 +1,20 @@
 const commonUtil = require('../util/commonUtil');
 const SeasonRepository = require('../repository/seasonRepository');
 const StatsSeasonRepository = require('../repository/statsSeasonRepository');
+const RecentCharLeaderStatsRepository = require('../repository/recentCharLeaderStatsRepository');
 
 module.exports = (scheduler, maria, acclogger) => {
     const app = require('express').Router();
     const seasonRepository = new SeasonRepository(maria);
     const statsSeasonRepository = new StatsSeasonRepository(maria);
+    const recentCharLeaderStatsRepository = new RecentCharLeaderStatsRepository(maria);
     app.use(acclogger());
 
     const DEFAULT_MIN_MATCHES = 30;
     const DEFAULT_HIDDEN_OP_EXCLUDE_RANK = 20;
     const SIDE_LIMIT = 10;
 
-    const getCurrentSeason = async () => {
+    const getCurrentSeason = async() => {
         const currentSeason = await seasonRepository.selectCurrentSeason();
         if (!currentSeason) {
             throw new Error('Current season metadata not found');
@@ -40,39 +42,31 @@ module.exports = (scheduler, maria, acclogger) => {
             }
         } catch (err) {
             logger.error(err);
-            return res
-                .status(500)
-                .send('season metadata not found')
-                .end();
+            return res.status(500).send('season metadata not found').end();
         }
     });
 
-    app.get('/statsCountList', async function(req, res) {        
-
+    app.get('/statsCountList', async function(req, res) {
         try {
             const currentSeason = await getCurrentSeason();
-            let query = ` SELECT dates, COUNT(dates) cnt FROM ( 
-             	SELECT DATE_FORMAT(matchDate, '%Y-%m-%d (%W)') dates, matchId FROM matches
+            const query = ` SELECT dates, COUNT(dates) cnt FROM (
+                SELECT DATE_FORMAT(matchDate, '%Y-%m-%d (%W)') dates, matchId FROM matches
                 WHERE matchDate >= ?
-             ) aa  
-             GROUP BY dates 
-             ORDER BY dates DESC 
+             ) aa
+             GROUP BY dates
+             ORDER BY dates DESC
              LIMIT 30 `;
 
-            let row = await maria.doQuery({ bigNumberStrings: true, sql: query }, [currentSeason.season_start_at]);
-            res.send({ 'row': row });
+            const row = await maria.doQuery({ bigNumberStrings: true, sql: query }, [currentSeason.season_start_at]);
+            res.send({ row });
         } catch (err) {
             logger.error(err);
             console.log(err);
-            return res
-                .status(500)
-                .send('오류 발생')
-                .end();
+            return res.status(500).send('?ㅻ쪟 諛쒖깮').end();
         }
     });
 
-    app.get('/statsCharList', async function(req, res) {        
-
+    app.get('/statsCharList', async function(req, res) {
         try {
             const currentSeason = await getCurrentSeason();
             const targetDate = req.query.date || commonUtil.getYYYYMMDD(commonUtil.addDays(new Date(), -1), false);
@@ -84,18 +78,60 @@ module.exports = (scheduler, maria, acclogger) => {
                 order by rate desc
             `;
             const row = await maria.doQuery({ bigNumberStrings: true, sql: query }, [targetDate, currentSeason.season_start_date]);
-            res.send({ 'row': row });
+            res.send({ row });
         } catch (err) {
             logger.error(err);
             console.log(err);
-            return res
-                .status(500)
-                .send('오류 발생')
-                .end();
+            return res.status(500).send('?ㅻ쪟 諛쒖깮').end();
         }
     });
 
-    // 메인 (/stats/statsList)
+    app.get('/statsRecentCharLeaders', async function(req, res) {
+        try {
+            const currentSeason = await getCurrentSeason();
+            const rows = await recentCharLeaderStatsRepository.selectLatestStats(currentSeason.season_code);
+
+            const row = [30, 7].map((periodDays) => ({
+                periodDays,
+                latestRankDate: rows.find((item) => Number(item.periodDays) === periodDays)?.latestRankDate || null,
+                list: rows
+                    .filter((item) => Number(item.periodDays) === periodDays)
+                    .map((item) => ({
+                        char_name: item.charName,
+                        total_matches: Number(item.totalMatches),
+                        win_count: Number(item.winCount),
+                        lose_count: Number(item.loseCount),
+                        rate: Number(item.rate),
+                        leaders: item.leadersJson ? JSON.parse(item.leadersJson) : []
+                    }))
+            }));
+
+            res.send({ row });
+        } catch (err) {
+            logger.error(err);
+            return res.status(500).send('?ㅻ쪟諛쒖깮').end();
+        }
+    });
+
+    app.get('/rebuildRecentCharLeaderStats', async function(req, res) {
+        if (!commonUtil.isMe(req)) {
+            return res.send({ resultCode: '400', resultMsg: '내가 아닌데??' });
+        }
+
+        try {
+            const currentSeason = await getCurrentSeason();
+            await recentCharLeaderStatsRepository.rebuildStats({
+                seasonCode: currentSeason.season_code,
+                seasonStartAt: currentSeason.season_start_at
+            });
+
+            return res.send({ resultCode: '200', resultMsg: 'OK' });
+        } catch (err) {
+            logger.error(err);
+            return res.status(500).send({ resultCode: '500', resultMsg: '오류발생' });
+        }
+    });
+
     app.get('/statsList', async function(req, res) {
         let todayStr = '';
 
@@ -111,68 +147,61 @@ module.exports = (scheduler, maria, acclogger) => {
 
         try {
             const currentSeason = await getCurrentSeason();
-            let query = ` SELECT * FROM char_combi_stats_ranked WHERE stat_date = ? AND stat_date >= ? `;
-            // let query = ` SELECT * FROM match_stats WHERE statsDate = ? `;
+            const query = ` SELECT * FROM char_combi_stats_ranked WHERE stat_date = ? AND stat_date >= ? `;
             logger.debug(query);
 
-            let row = await maria.doQuery(query, [todayStr, currentSeason.season_start_date]);
-            res.send({ 'row': row });
+            const row = await maria.doQuery(query, [todayStr, currentSeason.season_start_date]);
+            res.send({ row });
         } catch (err) {
             logger.error(err);
-            return res
-                .status(500)
-                .send('오류 발생')
-                .end();
+            return res.status(500).send('?ㅻ쪟 諛쒖깮').end();
         }
     });
 
     app.get('/stats2022h', function(req, res) {
-        res.render('./pc/statsSeason', { 'season': '2022H' });
+        res.render('./pc/statsSeason', { season: '2022H' });
     });
 
     app.get('/stats2022u', function(req, res) {
-        res.render('./pc/statsSeason', { 'season': '2022U' });
+        res.render('./pc/statsSeason', { season: '2022U' });
     });
 
     app.get('/stats2023h', function(req, res) {
-        res.render('./pc/statsSeason', { 'season': '2023H' });
+        res.render('./pc/statsSeason', { season: '2023H' });
     });
 
     app.get('/stats2023u', function(req, res) {
-        res.render('./pc/statsSeason', { 'season': '2023U' });
+        res.render('./pc/statsSeason', { season: '2023U' });
     });
 
     app.get('/stats2024h', function(req, res) {
-        res.render('./pc/statsSeason', { 'season': '2024H' });
+        res.render('./pc/statsSeason', { season: '2024H' });
     });
 
     app.get('/stats2024u', function(req, res) {
-        res.render('./pc/statsSeason', { 'season': '2024U' });
+        res.render('./pc/statsSeason', { season: '2024U' });
     });
 
     app.get('/stats2025u', function(req, res) {
-        res.render('./pc/statsSeason2025u', { 'season': '2025U' });
+        res.render('./pc/statsSeason2025u', { season: '2025U' });
     });
 
     app.get('/stats2025h', function(req, res) {
-        res.render('./pc/statsSeason2025u', { 'season': '2025H' });
+        res.render('./pc/statsSeason2025u', { season: '2025H' });
     });
 
     app.get('/statsSeasonList', async function(req, res) {
-        let season = req.query.season
+        const season = req.query.season;
 
         try {
-            let query = ` SELECT * FROM match_stats WHERE statsDate = '${season}' `;
+            const query = ` SELECT * FROM match_stats WHERE statsDate = '${season}' `;
             logger.debug(query);
 
-            let row = await maria.doQuery(query);
-            res.send({ 'row': row });
+            const row = await maria.doQuery(query);
+            res.send({ row });
         } catch (err) {
             logger.error(err);
-            return res
-                .status(500)
-                .send('오류 발생')
-                .end();
+            return res.status(500).send('?ㅻ쪟 諛쒖깮').end();
         }
     });
 
@@ -180,10 +209,7 @@ module.exports = (scheduler, maria, acclogger) => {
         const season = normalizeSeason(req.query.season || '2025U');
 
         if (!season) {
-            return res
-                .status(400)
-                .send('잘못된 시즌 코드')
-                .end();
+            return res.status(400).send('?섎せ???쒖쫵 肄붾뱶').end();
         }
 
         try {
@@ -217,10 +243,7 @@ module.exports = (scheduler, maria, acclogger) => {
             });
         } catch (err) {
             logger.error(err);
-            return res
-                .status(500)
-                .send('오류 발생')
-                .end();
+            return res.status(500).send('?ㅻ쪟 諛쒖깮').end();
         }
     });
 
@@ -231,19 +254,16 @@ module.exports = (scheduler, maria, acclogger) => {
         }
 
         try {
-            let query = ` SELECT * FROM char_stats where season = '${season}' ORDER BY rate DESC `;
+            const query = ` SELECT * FROM char_stats where season = '${season}' ORDER BY rate DESC `;
             logger.debug(query);
 
-            let row = await maria.doQuery(query);
-            res.send({ 'row': row });
+            const row = await maria.doQuery(query);
+            res.send({ row });
         } catch (err) {
             logger.error(err);
-            return res
-                .status(500)
-                .send('오류 발생')
-                .end();
+            return res.status(500).send('?ㅻ쪟 諛쒖깮').end();
         }
     });
 
     return app;
-}
+};
